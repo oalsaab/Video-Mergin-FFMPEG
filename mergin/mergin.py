30957c4632b746d286b882ca0926826d41f8f624
oalsaab <omaralsaab96@gmail.com>
2024-01-08 22:04:39 +0000

    Updated main method with imports for writer and generating a random uuid string for file name for input to ffmpeg

--- mergin/mergin.py
+++ mergin/mergin.py
@@ -1,17 +1,25 @@
 import asyncio
+import uuid
 from pathlib import Path
 
 from consumer import consumer
+from merger import writer
 from producer import producer
 
 
-async def main(files: Path):
+async def main(directory: Path):
     queue = asyncio.PriorityQueue()
+    name = uuid.uuid4()
 
-    producers = [asyncio.create_task(producer(queue, file)) for file in files.iterdir()]
+    producers = [
+        asyncio.create_task(producer(queue, file)) for file in directory.iterdir()
+    ]
+
     await asyncio.gather(*producers)
 
-    consumed = await asyncio.create_task(consumer(queue))
+    consumed = consumer(queue)
+
+    await writer(directory, name, consumed)
 
     await queue.join()
 
