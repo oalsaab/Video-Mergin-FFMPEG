d17cf88419228e0115b43872503407ef5fd7f40d
oalsaab <omaralsaab96@gmail.com>
2024-01-08 22:36:43 +0000

    Added functionality to merge files using subprocess and generated input txt file

--- mergin/merger.py
+++ mergin/merger.py
@@ -1,3 +1,5 @@
+import shlex
+import subprocess
 from pathlib import Path
 from typing import AsyncIterator
 
@@ -6,3 +8,14 @@ async def writer(file_input: Path, consumed: AsyncIterator[str]):
     with open(file_input, "w") as fp:
         async for item in consumed:
             fp.write(item)
+
+
+def merger(directory: Path, file_input: Path, file_output: Path):
+    cmd = shlex.split(
+        f"ffmpeg "
+        f"-hide_banner -loglevel error -f concat -safe 0 "
+        f"-i {file_input.name} "
+        f"-c copy {file_output.name}"
+    )
+
+    subprocess.run(cmd, cwd=directory)
