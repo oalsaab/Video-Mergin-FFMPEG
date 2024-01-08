0ad6c831981c9965273c355af64f44edfe796c1b
oalsaab <omaralsaab96@gmail.com>
2024-01-08 21:30:02 +0000

    Updated consumer to return list of filenames, to be further consumed by ffmpeg eventually

--- mergin/consumer.py
+++ mergin/consumer.py
@@ -3,12 +3,15 @@ from asyncio import PriorityQueue
 from producer import Work
 
 
-async def consumer(queue: PriorityQueue):
+async def consumer(queue: PriorityQueue) -> list[str]:
+    ordered = []
+
     while not queue.empty():
         item: Work = await queue.get()
 
-        # Write contents to file or list
-
-        print(f"Consuming {item.file} --> {item.creation}")
-
+        ordered.append(f"file '{item.file.name}'")
         queue.task_done()
+
+        print(f"Consuming {item.file.name} --> {item.creation}")
+
+    return ordered
