e70e3f8982e9e80b3ce96e061b01e227292342e3
oalsaab <omaralsaab96@gmail.com>
2024-01-14 02:18:20 +0000

    Updated consumer to include writer with added functionality of separating multimedia file per compatible mergeable files

--- mergin/consumer.py
+++ mergin/consumer.py
@@ -1,12 +1,27 @@
 from asyncio import PriorityQueue
+from collections import defaultdict
+from pathlib import Path
 from typing import AsyncIterator
 
 from producer import Work
 
 
-async def consumer(queue: PriorityQueue) -> AsyncIterator[str]:
+async def consumer(queue: PriorityQueue) -> AsyncIterator[Work]:
     while not queue.empty():
         item: Work = await queue.get()
 
-        yield f"file '{item.file.name}'\n"
+        yield item
         queue.task_done()
+
+
+async def writer(directory: Path, consumed: AsyncIterator[Work]) -> list[str]:
+    streams = defaultdict(list)
+
+    async for item in consumed:
+        streams[item.key].append(f"file '{item.file.name}'\n")
+
+    for stream, inputs in streams.items():
+        with open(Path(f"{directory}/{stream}.txt"), "w") as file:
+            file.writelines(inputs)
+
+    return list(streams.keys())
