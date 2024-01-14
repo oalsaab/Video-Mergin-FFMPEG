eebc55a6cf91bf3d30ec928ccf30bc73fe1af404
oalsaab <omaralsaab96@gmail.com>
2024-01-14 23:05:35 +0000

    Updated merger with temporary multi merge to perform merging through multiprocess

--- mergin/merger.py
+++ mergin/merger.py
@@ -1,16 +1,25 @@
 import shlex
 import subprocess
+from functools import partial
+from multiprocessing import Pool
 from pathlib import Path
 
 
-# You can most likely remove the -safe 0 parameter here,
+def multi_merge(merge_path: Path, inputs: list[str]):
+    uniques = len(inputs)
+    process = partial(merger, merge_path)
+
+    with Pool(uniques) as pool:
+        pool.map(process, inputs)
+
+
 # Read the docs on concat, add notes in readme that it is a demuxer / muxer.
-def merger(directory: Path, file_input: Path, file_output: Path):
+def merger(merge_path: Path, txt_input: str):
     cmd = shlex.split(
         f"ffmpeg "
         f"-hide_banner -loglevel error -f concat -safe 0 "
-        f"-i {file_input.name} "
-        f"-c copy {file_output.name}"
+        f"-i {txt_input}.txt "
+        f"-c copy {txt_input}.mkv"
     )
 
-    subprocess.run(cmd, cwd=directory)
+    subprocess.run(cmd, cwd=merge_path)
