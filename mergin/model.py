8d8f02469fff573e906a752d1b6b3337ee850bbe
oalsaab <omaralsaab96@gmail.com>
2024-01-14 00:44:36 +0000

    Updated Model with rename of stream & addition of more fields to decode - adding dimensions and codecs

--- mergin/model.py
+++ mergin/model.py
@@ -1,15 +1,40 @@
+from collections.abc import Iterator
 from datetime import datetime
+from datetime import timezone
 
 from msgspec import Struct
 
 
 class Tags(Struct):
-    creation_time: datetime
+    creation_time: datetime = datetime.fromtimestamp(0, timezone.utc)
 
 
 class Format(Struct):
     tags: Tags
 
 
-class Streams(Struct):
+class Stream(Struct):
+    codec_name: str
+    codec_type: str
+    width: int | None = None
+    height: int | None = None
+
+
+class MultiMedia(Struct):
     format: Format
+    streams: list[Stream]
+
+    @property
+    def video(self) -> Stream | None:
+        for stream in self.streams:
+            if stream.codec_type == "video":
+                return Stream
+
+        raise None
+
+    def __len__(self) -> int:
+        return len(self.streams)
+
+    def __iter__(self) -> Iterator:
+        for stream in self.streams:
+            yield stream
