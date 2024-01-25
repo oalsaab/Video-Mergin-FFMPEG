63c04aacf49ef45c17adaac93278d4b864c3080c
oalsaab <omaralsaab96@gmail.com>
2024-01-25 23:27:31 +0000

    Rename of merge function to private function

--- mergin/merger.py
+++ mergin/merger.py
@@ -19,7 +19,7 @@ class Result(NamedTuple):
 def merger(merge_path: Path, inputs: list[str]):
     logging.info("Initiating Merges...")
     uniques = len(inputs)
-    process = partial(merge, merge_path)
+    process = partial(_merge, merge_path)
 
     with Pool(uniques) as pool:
         for result in pool.imap_unordered(process, inputs):
@@ -28,7 +28,7 @@ def merger(merge_path: Path, inputs: list[str]):
 
 
 # Read the docs on concat, add notes in readme that it is a demuxer / muxer.
-def merge(merge_path: Path, txt_input: str) -> Result:
+def _merge(merge_path: Path, txt_input: str) -> Result:
     cmd = shlex.split(
         f"ffmpeg "
         f"-hide_banner -loglevel error -f concat -safe 0 "
