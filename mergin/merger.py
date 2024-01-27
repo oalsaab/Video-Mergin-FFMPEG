ee4797a96af8c0fa5cb762334d9f02b7d4ec8d4e
oalsaab <omaralsaab96@gmail.com>
2024-01-27 00:37:07 +0000

    Added logic to clean up input files and write inputs to final merged result file

--- mergin/merger.py
+++ mergin/merger.py
@@ -1,10 +1,13 @@
 import logging
 import shlex
 import subprocess
+from contextlib import contextmanager
 from functools import partial
 from multiprocessing import Pool
 from pathlib import Path
+from typing import Iterator
 from typing import NamedTuple
+from typing import TextIO
 
 from .model import Stream
 
@@ -25,7 +28,7 @@ class Result(NamedTuple):
         header = " || ".join(info)
         border = "=" * (len(header) + 4)
 
-        return f"\n{border}\n| {header} |\n{border}\n"
+        return f"{border}\n| {header} |\n{border}\n"
 
     def __str__(self) -> str:
         status = "Successful" if bool(self) else "Failed"
@@ -33,6 +36,17 @@ class Result(NamedTuple):
 
     def __bool__(self) -> bool:
         return self.code == 0
+
+
+@contextmanager
+def cleanup(path: Path, mode: str) -> Iterator[TextIO]:
+    file = open(path, mode)
+
+    try:
+        yield file
+    finally:
+        file.close()
+        path.unlink()
 
 
 def merger(merge_path: Path, inputs: list[str]):
@@ -43,8 +57,14 @@ def merger(merge_path: Path, inputs: list[str]):
     with Pool(uniques) as pool:
         for result in pool.imap_unordered(process, inputs):
             logging.info(result)
-            logging.info(result.create_header())
-            pass
+
+            with open(Path(f"{merge_path}/result.txt"), "a") as outfile:
+                with cleanup(Path(f"{merge_path}/{result.inp}.txt"), "r") as infile:
+                    header = result.create_header()
+                    outfile.write(header)
+
+                    streams = infile.readlines()
+                    outfile.writelines(streams)
 
 
 # Read the docs on concat, add notes in readme that it is a demuxer / muxer.
