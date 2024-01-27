cf24ce9cfc141c72d5a71a752ede5bf8ffb52705
oalsaab <omaralsaab96@gmail.com>
2024-01-27 00:02:47 +0000

    Created method for header creation using field names of Stream model & values of Stream

--- mergin/merger.py
+++ mergin/merger.py
@@ -6,10 +6,26 @@ from multiprocessing import Pool
 from pathlib import Path
 from typing import NamedTuple
 
+from .model import Stream
+
 
 class Result(NamedTuple):
     code: int
     inp: str
+
+    def create_header(self) -> str:
+        values = self.inp.split("_")
+        fields = [*Stream.__struct_fields__, *["audio"]]
+
+        mapped = dict(zip(fields, values))
+        audio = "Y" if mapped.get("audio") == "audio" else "N"
+        mapped.update({"audio": audio})
+
+        info = (f"{k.upper()}: {v}" for k, v in mapped.items())
+        header = " || ".join(info)
+        border = "=" * (len(header) + 4)
+
+        return f"\n{border}\n| {header} |\n{border}\n"
 
     def __str__(self) -> str:
         status = "Successful" if bool(self) else "Failed"
@@ -27,6 +43,7 @@ def merger(merge_path: Path, inputs: list[str]):
     with Pool(uniques) as pool:
         for result in pool.imap_unordered(process, inputs):
             logging.info(result)
+            logging.info(result.create_header())
             pass
 
 
